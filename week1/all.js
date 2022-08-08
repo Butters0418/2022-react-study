77f3a5ecc9a83a4f7a18870272dcfd88dfa417c6
Butters <swki418@gmail.com>
2022-08-09 00:30:24 +0800

    add currency data

--- week1/all.js
+++ week1/all.js
@@ -1,18 +1,35 @@
 const { useState } = React;
+const currencyObj = [
+  {
+    country: '日本',
+    currencyRatio: 4.48,
+  },
+  {
+    country: '美金',
+    currencyRatio: 0.03,
+  },
+  {
+    country: '澳幣',
+    currencyRatio: 0.05,
+  },
+  {
+    country: '韓幣',
+    currencyRatio: 43.29,
+  },
+  {
+    country: '印尼幣',
+    currencyRatio: 493.94,
+  },
+];
 function App() {
+  const { currency, setCurrency } = useState(currencyObj);
   return (
     <>
       <h1>請輸入您要換的台幣</h1>
       <input type="text" placeholder="台幣" />
       <input type="button" value="計算" />
       <p>可以換算</p>
-      <ul>
-        <li>日幣：</li>
-        <li>美金：</li>
-        <li>澳幣：</li>
-        <li>韓幣：</li>
-        <li>印尼幣：</li>
-      </ul>
+      <ul></ul>
     </>
   );
 }
