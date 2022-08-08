13dafd5ff37a4101d5554c705e955382b370f7e6
Butters <swki418@gmail.com>
2022-08-08 23:55:32 +0800

    week1

--- week1/all.js
+++ week1/all.js
@@ -1,7 +1,21 @@
-const numbers = [1, 2, 3, 4, 5];
-const numberList = numbers.map((numbers, i) => {
-  return <li key={i}>{numbers}</li>;
-});
+const { useState } = React;
+function App() {
+  return (
+    <>
+      <h1>請輸入您要換的台幣</h1>
+      <input type="text" placeholder="台幣" />
+      <input type="button" value="計算" />
+      <p>可以換算</p>
+      <ul>
+        <li>日幣：</li>
+        <li>美金：</li>
+        <li>澳幣：</li>
+        <li>韓幣：</li>
+        <li>印尼幣：</li>
+      </ul>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ul>{numberList}</ul>);
+root.render(<App />);
